f1b903292328a94b183fe9c6ac4cf88da9e44ad6
Flammable-Duck <flammableduck@protonmail.com>
2021-11-09 19:04:49 -0800

    moved everything into an object

--- script.js
+++ script.js
@@ -1,116 +1,111 @@
-const Display = document.querySelector('.display')
-let inputValue = 0.0
-let lastValue = 0.0
-let operation = ""
-let solution = null
-let isDecimal = false
-
-function calculate() {
-    switch (operation){
-        case "+":
-            solution = inputValue + lastValue;
-            break;
-        case "-":
-            solution = lastValue - inputValue;
-            break;
-        case "*":
-            solution = lastValue * inputValue;
-            break;
-        case "/":
-            solution = lastValue / inputValue;
-            break;
+class Calculator {
+    constructor() {
+        this.displayElement = document.querySelector(".display");
+        this.inputValue = 0.0
+        this.lastValue = 0.0
+        this.operation = ""
+        this.solution = null
+        this.isDecimal = false
     }
-    solution = Math.round(solution*1000000)/1000000
-}
-
-function equals() {
-    if (!lastValue && solution) {
-        // this allows users to input another calculation using the past result
-        lastValue = solution
+    calculate() {
+        switch (this.operation){
+            case "+":
+                this.solution = this.inputValue + this.lastValue;
+                break;
+            case "-":
+                this.solution = this.lastValue - this.inputValue;
+                break;
+            case "*":
+                this.solution = this.lastValue * this.inputValue;
+                break;
+            case "/":
+                this.solution = this.lastValue / this.inputValue;
+                break;
+        }
+        this.solution = Math.round(this.solution*1000000)/1000000;
     }
-    if (inputValue && lastValue && operation) {
-        calculate()
-        display(solution)
-        inputValue = ""
-        return
-    }
-}
-
-function input(input) {
-    if (isDecimal == true && input != '.') {
-        isDecimal = false;
-            console.log('bar')
-        if (!inputValue) {
-            input = `0.${input}`;
-            console.log('foo')
-            console.log(input)
-        } else {
-            input = `.${input}`;
+    equals() {
+        if (!this.lastValue && this.solution) {
+            // this allows users to input another calculation using the past result
+            this.lastValue = this.solution;
+        }
+        if (this.inputValue && this.lastValue && this.operation) {
+            this.calculate();
+            this.display(this.solution);
+            this.inputValue = "";
+            return;
         }
     }
-    if (input == '.') {isDecimal = true}
-    inputValue = parseFloat(`${inputValue}${input}`)
-    display(inputValue)
-}
-
-function setOperator(operator) {
-    if (operator.value == "=") {equals(); return}
-    display(operator.value)
-    operation= operator.value
-    lastValue = inputValue
-    inputValue = ""
-    equals()
-}
-
-function doFunctions(func) {
-    console.log(func.value)
-    switch (func.value) {
-        case 'c':
-            clear();
-            break;
-        case '%':
-            inputValue /= 100
-            display(inputValue)
-            break;
-        case 'sign':
-            inputValue *= -1
-            display(inputValue)
+    input(input) {
+        if (this.isDecimal == true && input != '.') {
+            this.isDecimal = false;
+            if (!this.inputValue) {
+                input = `0.${input}`;
+            } else {
+                input = `.${input}`;
+            }
+        }
+        if (input == '.') {this.isDecimal = true};
+        this.inputValue = parseFloat(`${this.inputValue}${input}`);
+        this.display(this.inputValue);
+    }
+    setOperator(operator) {
+        if (operator.value == "=") {this.equals(); return};
+        this.display(operator.value);
+        this.operation= operator.value;
+        this.lastValue = this.inputValue;
+        this.inputValue = "";
+        this.equals();
+    }
+    doFunctions(func) {
+        switch (func.value) {
+            case 'c':
+                this.clear();
+                break;
+            case '%':
+                this.inputValue /= 100
+                this.display(this.inputValue)
+                break;
+            case 'sign':
+                this.inputValue *= -1
+                this.display(this.inputValue)
+        }
+    }
+    clear() {
+        this.display.textContent = "";
+        this.inputValue = 0.0;
+        this.lastValue = 0.0;
+        this.operation = "";
+        this.solution = null;
+    }
+    display(input) {
+        this.displayElement.textContent = input;
     }
 }
 
-function clear() {
-    Display.textContent = ""
-    inputValue = 0.0
-    lastValue = 0.0
-    operation = ""
-    solution = null
-}
-
-function display(input) {
-    Display.textContent = input
-}
-
-function attachNums() {
+function attachNums(calculator) {
     const numberButtons = document.querySelectorAll('.calculator .Number')
     numberButtons.forEach(numberButton => numberButton.addEventListener('click', function(){
-        input(numberButton.value)
+        calculator.input(numberButton.value)
     }))
 }
 
-function attachOperators() {
+function attachOperators(calculator) {
     const operators = document.querySelectorAll('.calculator .Operator')
     operators.forEach(operator => operator.addEventListener('click', function(){
-        setOperator(operator)
+        calculator.setOperator(operator)
     }))
 }
 
-function attachFunctions() {
+function attachFunctions(calculator) {
     const functions = document.querySelectorAll('.calculator .function')
     functions.forEach(functions => functions.addEventListener('click', function(){
-        doFunctions(functions)
+        calculator.doFunctions(functions)
     }))
 }
 
-attachFunctions()
-attachNums()
-attachOperators()
+const calculator = new Calculator();
+
+attachFunctions(calculator);
+attachNums(calculator);
+attachOperators(calculator);
